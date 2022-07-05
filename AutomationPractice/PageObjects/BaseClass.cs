0ab8fc0f9fa266bdfe4406c03893941f1cc620c0
cecelia-tech <vitagriciute@gmail.com>
2022-07-05 15:42:57 +0300

    ValidateDropdownOptions added

--- AutomationPractice/PageObjects/BaseClass.cs
+++ AutomationPractice/PageObjects/BaseClass.cs
@@ -44,4 +44,74 @@ public abstract class BaseClass
     }
 
     public void Logout() => ClickElement(logout);
+
+    protected bool IsElementVisible(By element)
+    {
+        try
+        {
+            return new WebDriverWait(BrowserEnvironment.Driver, TimeSpan.FromSeconds(10))
+            .Until(ExpectedConditions.ElementIsVisible(element)).Displayed;
+
+        }
+        catch (WebDriverTimeoutException)
+        {
+            return false;
+        }
+    }
+
+    protected bool IsElementClickable(IWebElement element)
+    {
+        try
+        {
+            new WebDriverWait(BrowserEnvironment.Driver, TimeSpan.FromSeconds(10))
+                .Until(ExpectedConditions.ElementToBeClickable(element));
+
+            return true;
+        }
+        catch (WebDriverTimeoutException)
+        {
+            return false;
+        }
+    }
+
+    protected bool HasElementTextChanged(IWebElement element)
+    {
+        try
+        {
+            new WebDriverWait(BrowserEnvironment.Driver, TimeSpan.FromSeconds(10))
+                .Until(ExpectedConditions.ElementToBeClickable(element));
+
+            return true;
+        }
+        catch (WebDriverTimeoutException)
+        {
+            return false;
+        }
+    }
+
+    internal void SwitchToFrame(IWebElement frame)
+    {
+        BrowserEnvironment.Driver.SwitchTo().Frame(frame);
+    }
+
+    internal void ExitFrame()
+    {
+        BrowserEnvironment.Driver.SwitchTo().DefaultContent(); ;
+    }
+
+    protected bool ValidateDropdownOptions(IWebElement dropdownElement, IEnumerable<string> expectedOptions)
+    {
+        SelectElement dropdown = new SelectElement(dropdownElement);
+
+        return expectedOptions.Count() == dropdown.Options.Count ? dropdown.Options.Select(option => option.Text).Except(expectedOptions).Any() : false;
+
+        //if (expectedOptions.Count() != dropdown.Options.Count)
+        //{
+        //    return false;
+        //}
+
+        //var actualOptions = dropdown.Options.Select(option => option.Text);
+
+        //return actualOptions.Except(expectedOptions).Any();
+    }
 }
